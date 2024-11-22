9e3a3c3c1555a42fe5a6ba0702a2c80fcbb761fa
EricPlasenciaD <franciscoeric777@gmail.com>
2024-11-22 16:08:07 +0000

    añadidos suma y resta

---
+++ Resta.py
@@ -0,0 +1,13 @@
+
+print ("\nElija dos numeros para restar:")
+
+try:
+        num1 = float(input("introduzca un número: "))
+        num2 = float(input("introduzca otro número: "))
+except ValueError:
+        print ("Introduzca un valor numérico")
+        exit()
+   
+resultado = num1 - num2
+
+print (f"el resultado de la operación es: {resultado}")
